2aa3b71912154252272608ef89161fb11ca8a615
Gabriel Ford <gabe@fordltc.net>
2022-01-08 00:27:00 -0500

    commiting #2

---
+++ FindTheDivisors.js
@@ -0,0 +1,19 @@
+function divisors(integer) {
+    var lst = Array();
+    for (index = 2; index < integer; index++) {
+        if (integer % index == 0) {
+            lst.push(index);
+            console.log(index + " divides");
+        } else {
+            console.log(index + " does not divide");
+        }
+    }
+    if (lst.length > 0) {
+        return lst;
+    } else {
+        return integer + ' is prime';
+    }
+};
+console.log(divisors(27));
+
+// It passed!!!!
